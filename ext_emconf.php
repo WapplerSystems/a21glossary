4f41a5f8888f029049bf9e8dfa9d935acc106ac6
Sven Wappler <info@wappler.systems>
2018-02-16 17:54:39 +0100

    New version 8.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,19 +30,19 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'WapplerSystems',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '8.0.0',
+	'version' => '8.0.1',
 	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"d6a8";s:24:"class.tx_a21glossary.php";s:4:"16f1";s:16:"ext_autoload.php";s:4:"5489";s:21:"ext_conf_template.txt";s:4:"76ef";s:12:"ext_icon.gif";s:4:"dff2";s:17:"ext_localconf.php";s:4:"b780";s:15:"ext_php_api.dat";s:4:"a346";s:14:"ext_tables.php";s:4:"382e";s:14:"ext_tables.sql";s:4:"f3ae";s:28:"icon_tx_a21glossary_main.gif";s:4:"a4ed";s:13:"locallang.xml";s:4:"efca";s:16:"locallang_db.xml";s:4:"4280";s:21:"Configuration/Tca.php";s:4:"36e1";s:52:"Configuration/TypoScript/Accessibility/constants.txt";s:4:"8dbf";s:48:"Configuration/TypoScript/Accessibility/setup.txt";s:4:"9a24";s:14:"doc/manual.sxw";s:4:"f0a4";s:20:"static/constants.txt";s:4:"26fe";s:16:"static/setup.txt";s:4:"df90";}',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => [
+		'depends' => [
 			'typo3' => '8.7.0-8.7.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
+        ],
+		'conflicts' => [
+        ],
+		'suggests' => [
+        ],
+    ],
+	'suggests' => [
+    ],
 );
 
 ?>
