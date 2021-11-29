ac54549dcfbac976682b147335546f004d6eca4b
Thorben Nissen <thorben@webcoast.dk>
2021-11-29 13:37:42 +0100

    [TASK] Prepare for v11
    * raise TYPO3 dependency to ^10.4 or ^11.5
    * cleanup `ext_emconf.php`

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,34 +13,25 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'A21 Glossary',
     'description' => 'A21 Glossary - automatic conversion of all abbreviations and acronyms in the special tags for accessibility issues',
+    'version' => '11.0.0',
     'category' => 'fe',
-    'shy' => 0,
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
-    'state' => 'beta',
-    'internal' => 0,
-    'uploadfolder' => 0,
-    'clearCacheOnLoad' => 1,
-    'lockType' => '',
-    'author' => 'Sven Wappler',
-    'author_email' => 'typo3YYYY@wappler.systems',
-    'author_company' => 'WapplerSystems',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
-    'version' => '10.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.99-11.5.99',
         ],
         'conflicts' => [
         ],
         'suggests' => [
         ],
     ],
-    'suggests' => [
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'autoload' => [
+        'psr-4' => [
+            'WapplerSystems\\A21glossary\\' => 'Classes'
+        ]
     ],
 ];
-
