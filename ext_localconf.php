003191ff3674473fb1ee2d0bdc1c75b4da28e985
Thorben Nissen <thorben@webcoast.dk>
2021-12-13 12:22:58 +0100

    [BUGFIX] Fix plugin configuration, table mapping and translations
    * use full controller class name in `ExtensionUtility::configurePlugin`
    * add `Configuration/Extbase/Persistence/Classes.php` with table mapping configuration
    * convert `xml` to `xlf` language file and split into to files

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,7 +11,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPo
 TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'SveWap.A21glossary',
     'Pi1',
-    ['Glossary' => 'index,search'],
-    ['Glossary' => 'search']
+    [\WapplerSystems\A21glossary\Controller\GlossaryController::class => 'index,search'],
+    [\WapplerSystems\A21glossary\Controller\GlossaryController::class => 'search']
 );
-
