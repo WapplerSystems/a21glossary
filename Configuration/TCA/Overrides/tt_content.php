dd40bd304f2ee53b8f2334b184f2490a5b97495e
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-01-26 23:29:21 +0100

    [TASK] EMUtility::addToInsertRecords should be in Configuration/TCA

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,7 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 ExtensionUtility::registerPlugin('SveWap.A21glossary', 'Pi1', 'LLL:EXT:a21glossary/Resources/Private/Language/locallang_db.xml:plugins.pi1.title');
+ExtensionManagementUtility::addToInsertRecords('tx_a21glossary_main');
--- ext_tables.php
+++ ext_tables.php
@@ -6,4 +6,3 @@ if (!defined('TYPO3_MODE')) {
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_a21glossary_main');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tx_a21glossary_main');
