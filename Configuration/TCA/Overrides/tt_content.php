d30948daf26e3ebc2eeb78c71d9f8250a3cff3ba
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-07-31 01:10:02 +0200

    [BUGFIX] registerPlugin with vendor name
    Deprecation: #88995

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,5 +3,5 @@
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-ExtensionUtility::registerPlugin('SveWap.A21glossary', 'Pi1', 'LLL:EXT:a21glossary/Resources/Private/Language/locallang_db.xml:plugins.pi1.title');
+ExtensionUtility::registerPlugin('A21glossary', 'Pi1', 'LLL:EXT:a21glossary/Resources/Private/Language/locallang_db.xml:plugins.pi1.title');
 ExtensionManagementUtility::addToInsertRecords('tx_a21glossary_main');
