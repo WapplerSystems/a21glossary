5f8bef04c2db9a0fc49f06ca9c556c7a04a8ace8
Sven Wappler <info@wappler.systems>
2023-09-22 11:12:33 +0200

    update

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,10 +2,6 @@
 
 use WapplerSystems\A21glossary\Controller\GlossaryController;
 
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['tx_a21glossary'] = WapplerSystems\A21glossary\Hooks\FrontendHook::class . '->processHook';
 
--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,3 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_a21glossary_main');
