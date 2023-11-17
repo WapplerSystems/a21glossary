d4de8959de505062ca94a65a42cd640e49f3f99a
Sven Wappler <info@wappler.systems>
2023-11-17 10:06:34 +0100

    12.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,18 +10,14 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['a21glossary'] = [
     'title' => 'A21 Glossary',
     'description' => 'A21 Glossary - automatic conversion of all abbreviations and acronyms in the special tags for accessibility issues',
-    'version' => '11.0.4',
+    'version' => '12.0.0',
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
+            'typo3' => '12.4.0-12.4.99',
         ],
     ],
     'state' => 'stable',
